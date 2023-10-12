d9e33c8951c77128602a4872593332803b2750d1
s10043679 <fletcherbwilson1@gmail.com>
2023-10-12 09:44:39 -0500

    commit 2 filter screen started

--- main.py
+++ main.py
@@ -1,25 +1,136 @@
+from PIL import Image, ImageDraw
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
+from os import system
+
+system('pip install Pillow')
+import math
+import random
 
 
 class PhotoGalleryApp(App):
     pass
 
+
 class Display(Screen):
     def displayimage(self):
         return images[index]
+
     def advance(self):
         global index, images
         if index < len(images):
             index += 1
+        else:
+            index = 0
+        return images[index]
+
     def back(self):
         global index, images
-        if index > 0:
+        if index >= 1:
             index -= 1
+        else:
+            index = len(images)
+        return images[index]
 
 
+class Filters(Screen):
+    def black_and_white(self, image, name):
+        img = Image.open(image)
+        pixels = img.load()
+        for y in range(img.size[1]):
+            for x in range(img.size[0]):
+                red = pixels[x, y][0]
+                green = pixels[x, y][1]
+                blue = pixels[x, y][2]
 
-images = ['erosionbird.jfif''smurfcat.jfif']
+                avg = (red + blue + green) // 3
+
+                pixels[x, y] = (avg, avg, avg)
+
+        img.save(name + '_greyscale.png')
+
+    def invert(self, image, name):
+        img = Image.open(image)
+        pixels = img.load()
+        for y in range(img.size[1]):
+            for x in range(img.size[0]):
+                red = 255 - pixels[x, y][0]
+                green = 255 - pixels[x, y][1]
+                blue = 225 - pixels[x, y][2]
+                pixels[x, y] = (red, green, blue)
+        img.save(name + '_inverted.png')
+
+    def sepia(self, image, name):
+        img = Image.open(image)
+        pixels = img.load()
+        for y in range(img.size[1]):
+            for x in range(img.size[0]):
+                red = pixels[x, y][0]
+                green = pixels[x, y][1]
+                blue = pixels[x, y][2]
+
+                r1 = int(red * .393 + green * 0.769 + blue * 0.189)
+                g1 = int(red * .349 + green * 0.686 + blue * 0.168)
+                b1 = int(red * .272 + green * 0.534 + blue * 0.131)
+
+                pixels[x, y] = (r1, g1, b1)
+
+        img.save(name + '_sepia.png')
+
+    def pixelate(self, image, name, startx, starty, width, height):
+        img = Image.open(image)
+        pixels = img.load()
+
+        for y in range(starty, starty + height, 40):
+            for x in range(startx, startx + width, 40):
+                color = pixels[x, y]
+
+                for j in range(y, y + 40):
+                    for i in range(x, x + 40):
+                        pixels[i, j] = color
+
+        img.save(name + '_pixelated.png')
+
+    def pointillism(self, image, name):
+        img = Image.open(image)
+        pixels = img.load()
+        canvas = Image.new("RGB", (img.size[0], img.size[1]), "white")
+
+        for i in range(300000):
+            x = random.randint(0, img.size[0] - 1)
+            y = random.randint(0, img.size[1] - 1)
+            size = random.randint(6, 9)
+            ellipsebox = [(x, y), (x + size, y + size)]
+            draw = ImageDraw.Draw(canvas)
+            draw.ellipse(ellipsebox, fill=(pixels[x, y][0], pixels[x, y][1], pixels[x, y][2]))
+            del draw
+
+        canvas.save(name + '_points.png')
+
+    def lines_helper(self, pixel):
+        avg = (pixel[0] + pixel[1] + pixel[2]) // 3
+        return avg
+
+    def lines(self, image, name):
+        img = Image.open(image)
+        pixels = img.load()
+        canvas = Image.new("RGB", (img.size[0], img.size[1]), "white")
+
+        for y in range(img.size[1] - 1):
+            for x in range(img.size[0] - 1):
+                potency = self.lines_helper(pixels[x, y])
+                if abs(potency - self.lines_helper(pixels[x + 1, y])) > 20 or abs(
+                        potency - self.lines_helper(pixels[x - 1, y])) > 20 or abs(
+                    potency - self.lines_helper(pixels[x, y + 1])) > 20 or abs(
+                    potency - self.lines_helper(pixels[x, y - 1])) > 20:
+                    ellipsebox = [(x - 1, y - 1), (x, y)]
+                    draw = ImageDraw.Draw(canvas)
+                    draw.ellipse(ellipsebox, fill=(0, 0, 0))
+                    del draw
+
+        canvas.save(name + '_lines.png')
+
+
+images = ['erosionbird.jfif', 'smurfcat.jfif']
 index = 0
 PhotoGalleryApp().run()
-
